7da0c7894d6d0ce6f453a35ae1e919f2bb84a329
S-Arun0010 <123442099+S-Arun0010@users.noreply.github.com>
2023-02-07 12:51:35 +0530

    Add files via upload

---
+++ simplecalc/src/main/java/simplecalc/Calculator.java
@@ -0,0 +1,89 @@
+package simplecalc;
+import java.util.*;
+import java.util.logging.Logger;
+abstract class Calc
+{   
+    public abstract double calculate(double a,double b);
+}
+class Add extends Calc{
+    public double calculate(double a,double b)
+    {
+        return a+b;
+    }
+}
+class Sub extends Calc{
+    public double calculate(double a,double b)
+    {
+        return a-b;
+    }
+}
+class Mul extends Calc{
+    public double calculate(double a,double b)
+    {
+        return a*b;
+    }
+}
+class Div extends Calc{
+    public double calculate(double a,double b)
+    {
+        return a/b;
+    }
+}
+public class Calculator
+{
+	  public static double res = 0;
+
+	  public static void main (String[]args)
+	  {
+	    Logger log = Logger.getLogger ("com.api.jar");
+	    Scanner sc = new Scanner (System.in);
+	    log.info("To perform operation in this calculator follow the following format.....");
+	    log.info("First enter the number...");
+	    log.info("Second enter the any operator's among '+' '-' '*' '/'....");
+	    log.info("Eg, 10 + 20 - 5 * 2 / 2...");
+	    log.info ("To show the result press '=' operator...");
+	    double a = sc.nextInt ();
+	      res = a;
+	    double b = 0;
+	    char c = '0';
+	    while (c != '=')
+	      {
+	        	
+	        	char opp = sc.next ().charAt (0);
+	    	switch (opp)
+		     {
+		       case '+':
+		           
+		           b = sc.nextInt ();
+		           Calc i = new Add ();
+		           res = i.calculate (res, b);
+		           break;
+		       case '-':
+		           
+		           b = sc.nextInt ();
+		           Calc j = new Sub ();
+		           res = j.calculate (res, b);
+		           break;
+		       case '*':
+		           
+		           b = sc.nextInt ();
+		           Calc k = new Mul ();
+		           res = k.calculate (res, b);
+		           break;
+		       case '/':
+		           
+		           b = sc.nextInt ();
+		           Calc l = new Div ();
+		           res = l.calculate (res, b);
+		           break;
+		       case '=':
+		           c = '=';
+		           log.info  ("Result is :" + res);
+		           break;
+		       default:
+		           log.info  ("Enter valid operator:");
+		           break;
+		     }
+	      }
+	  }
+	}
